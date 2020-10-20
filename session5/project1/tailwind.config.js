fbb0dae05d8f6f82cd096c169d7e9be6995ff7d1
Saleem Hadad <saleem_ha@hotmail.com>
2020-10-20 08:37:13 +0400

    add session 5 and 6

---
+++ session5/project1/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    content: ['./public/**/*.html'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
